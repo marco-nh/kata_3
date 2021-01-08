3dbcba71cb512c3241fa8f5f0966b75d6d6a1172
usuario <usuario@MARCO-PC>
2021-01-08 10:27:50 +0000

    Final Version

---
+++ src/kata3_is2/Kata3_IS2.java
@@ -0,0 +1,24 @@
+package kata3_is2;
+
+public class Kata3_IS2 {
+
+    
+    public static void main(String[] args) {
+        
+        Histogram <String> histogram = new Histogram<>();
+        
+        histogram.increment("ulpgc.es");
+        histogram.increment("dis.ulpgc.es");
+        histogram.increment("ull.es");
+        histogram.increment("gmail.com");
+        histogram.increment("ull.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("gmail.com");
+        
+        
+        HistogramDisplay histogramDisplay = new HistogramDisplay("HISTOGRAM", histogram);
+        histogramDisplay.execute();
+        
+    }
+    
+}
